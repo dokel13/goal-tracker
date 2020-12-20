5be3afb05c6ec811acc309ef68149992d3e7dc9e
Oleksii Huts <Oleksii_Huts@epam.com>
2020-12-20 11:08:51 +0200

    TODO save tasks 4

--- src/main/java/com/tracker/goal/controller/UserController.java
+++ src/main/java/com/tracker/goal/controller/UserController.java
@@ -20,7 +20,6 @@ import static org.springframework.security.core.context.SecurityContextHolder.ge
 @RestController
 public class UserController extends UserGetter {
 
-    private UserService userService;
     private AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
--- src/main/java/com/tracker/goal/controller/utils/UserGetter.java
+++ src/main/java/com/tracker/goal/controller/utils/UserGetter.java
@@ -2,13 +2,15 @@ package com.tracker.goal.controller.utils;
 
 import com.tracker.goal.domain.User;
 import com.tracker.goal.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static org.springframework.security.core.context.SecurityContextHolder.getContext;
-
 @Component
 public abstract class UserGetter {
 
+    @Autowired
     protected UserService userService;
 
     protected User getUserFromContext() {
